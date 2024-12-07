0ae312e4753e02475db317ef997f921e2010ca7e
vboxuser <vboxuser@ubuntufossa.myguest.virtualbox.org>
2024-12-07 09:16:58 -0800

    darkmode functionality implemented

---
+++ app/javascript/client/dynamic.js
@@ -0,0 +1,22 @@
+let isDark = true;
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    const theme = localStorage.getItem('theme') || 'lightmode'
+    document.body.classList.add(theme)
+
+    const themeBtn = document.getElementById('themeBtn')
+    document.addEventListener('click', (el)=>{
+        
+        if(el.target.id == 'themeBtn'){
+            const actualTheme = document.body.classList.contains('lightmode') ? 'lightmode' : 'darkmode';
+            const newTheme = actualTheme === 'lightmode' ? 'darkmode': 'lightmode'
+            document.body.classList.remove(actualTheme)
+            document.body.classList.add(newTheme)
+            localStorage.setItem('theme', newTheme);
+
+            
+        }
+    })
+
+
+})
